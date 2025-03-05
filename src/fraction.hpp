dc25b77f59861a2fd759eda68e14eec2e4a6495f
Yohann LAI <>
2025-03-05 08:45:41 +0100

    First commit

---
+++ src/fraction.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+struct Fraction {
+    unsigned int numerator { 0 };
+    unsigned int denominator { 1 };
+
+    void display();
+};
+
+Fraction add(Fraction const& f1, Fraction const& f2);
+Fraction sub(Fraction const& f1, Fraction const& f2);
+Fraction mul(Fraction const& f1, Fraction const& f2);
+Fraction div(Fraction const& f1, Fraction const& f2);
---
+++ src/utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "fraction.hpp"
+
+unsigned int gcd(unsigned int a, unsigned int b);
+Fraction simplify(Fraction const& f);
+void simplify_ref(Fraction& f);
