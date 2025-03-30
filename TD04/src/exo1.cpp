fb0e730e8ce9fe8e08f6b07db204f72437fd0f13
Yohann LAI <>
2025-03-30 16:49:36 +0200

    Completion of exercise 1 of TD04

--- TD04/src/exo1.cpp
+++ TD04/src/exo1.cpp
@@ -2,10 +2,12 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
 int main() {
     std::vector<int> num_array(10);
 
+    // Code donné dans les "Retours généraux"
     std::random_device rd {}; // Objet permettant de générer une graine aléatoire (similaire à srand(time(nullptr)))
     std::mt19937 gen(rd()); // Générateur de nombres pseudo-aléatoires Mersenne Twister (un des générateurs les plus performants)
     std::uniform_int_distribution<> dis(0, 100); // Distribution uniforme entre 0 et 100
@@ -16,19 +18,20 @@ int main() {
         num_array[i] = random_number;
     }
     
-    std::vector<int>::iterator begin_iterator { num_array.begin() };
-    std::vector<int>::iterator end_iterator { num_array.end() };
+    std::vector<int>::iterator begin_it { num_array.begin() };
+    std::vector<int>::iterator end_it { num_array.end() };
 
-    std::cout << "Vecteur: [";
-    for (auto it = begin_iterator; it != end_iterator; it++)
+    // Affichage du vecteur avec les itérateurs
+    std::cout << "Vecteur : [";
+    for (auto it = begin_it; it != end_it; it++)
     {
-        if (*it != num_array.back())
+        if (it + 1 != end_it)
         {
             std::cout << *it << " ";
         }
         else
         {
-            std::cout << num_array.back() << "]\n";
+            std::cout << *it << "]\n";
         }
     }
 
@@ -38,6 +41,7 @@ int main() {
         std::cout << "Entrez un nombre entier entre 0 et 100 pour savoir s'il est dans le vecteur : ";
         std::cin >> value;
 
+        // Gestion de l'erreur pour la saisie des nombres
         if(std::cin.fail())
         {
             std::cin.clear();
@@ -46,19 +50,26 @@ int main() {
             break;
         }
 
-        auto finder { std::find(begin_iterator, end_iterator, value) };
-        if (finder != end_iterator)
+        // Chercher si le nombre est présent
+        auto finder { std::find(begin_it, end_it, value) };
+        if (finder != end_it)
         {
             std::cout << value << " est présent dans le vecteur.\n";
+
+            // Compter le nombre d'occurrences du nombre
+            int count = std::count(begin_it, end_it, value);
+            std::cout << value << " apparaît " << count << " fois dans le vecteur.\n";
         }
         else
         {
             std::cout << value << " n'est pas présent dans le vecteur.\n";
         }
 
+        // Pour continuer à chercher
         std::cout << "Continuer à chercher ?    1. Oui    2. Non\n";
         std::cin.clear();
         std::cin >> choice;
+        // Gestion de l'erreur pour le choix
         if(choice != 1 && choice != 2 || std::cin.fail())
         {
             std::cin.clear();
@@ -67,4 +78,23 @@ int main() {
             break;
         }
     } while (choice != 2);
+
+    // Trier le vecteur
+    std::sort(begin_it, end_it);
+    std::cout << "Vecteur trié : [";
+    for (auto it = begin_it; it != end_it; it++)
+    {
+        if (it + 1 != end_it)
+        {
+            std::cout << *it << " ";
+        }
+        else
+        {
+            std::cout << *it << "]\n";
+        }
+    }
+
+    // Sommer les éléments du vecteur
+    int sum = std::accumulate(begin_it, end_it, 0);
+    std::cout << "Somme des éléments du vecteur : " << sum << std::endl;
 }
