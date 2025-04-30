99c7973aa96e0c6d53237d6f00d352ee2c594558
Yohann LAI <>
2025-04-30 10:21:06 +0200

    Add the continuation of exercise 1 of TD07

--- TD07/src/graph.cpp
+++ TD07/src/graph.cpp
@@ -1,25 +1,47 @@
-#include "graph.hpp"
+#include "../include/graph.hpp"
 #include <iostream>
 
 // Exercice 1
 
-void WeightedGraph::add_vertex(int const id)
+namespace Graph
 {
-    if (adjacency_list.find(id) == adjacency_list.end())
+    void WeightedGraph::add_vertex(int const id)
     {
-        adjacency_list[id] = {};
+        if (adjacency_list.find(id) == adjacency_list.end())
+        {
+            adjacency_list[id] = {};
+        }
     }
-}
 
-void WeightedGraph::add_directed_edge(int const from, int const to, float const weight = 1.0f)
-{
-    add_vertex(from);
-    add_vertex(to);
-    adjacency_list[from].push_back({to, weight});
-}
+    void WeightedGraph::add_directed_edge(int const from, int const to, float const weight)
+    {
+        add_vertex(from);
+        add_vertex(to);
+        adjacency_list[from].push_back({to, weight});
+    }
 
-void WeightedGraph::add_undirected_edge(int const from, int const to, float const weight = 1.0f)
-{
-    add_directed_edge(from, to, weight);
-    add_directed_edge(to, from, weight);
+    void WeightedGraph::add_undirected_edge(int const from, int const to, float const weight)
+    {
+        add_directed_edge(from, to, weight);
+        add_directed_edge(to, from, weight);
+    }
+
+    WeightedGraph build_from_adjacency_matrix(std::vector<std::vector<float>> const& adjacency_matrix)
+    {
+        WeightedGraph graph;
+        int n = adjacency_matrix.size();
+        for (int from = 0; from < n; from++)
+        {
+            for (int to = 0; to < adjacency_matrix[from].size(); to++)
+            {
+                float weight = adjacency_matrix[from][to];
+                if (weight != 0.0f)
+                {
+                    graph.add_directed_edge(from, to, weight);
+                }
+            }
+        }
+
+        return graph;
+    }
 }
